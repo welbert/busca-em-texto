ecb4952d036f0ad92963c4e18795dce0618de844
Welbert Serra <welberts@gmail.com>
2014-10-10 19:40:17 -0300

    Refeito o corretor; Removidos algumas pogs; Todo: Fazer a lista encadeada, e testar entradas grandes

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 //-------------Estrutura
 typedef struct No{
 	char* exists;//Sinaliza se existe a palavra até esse ponto
@@ -13,11 +14,13 @@ typedef struct No{
 typedef struct Sugestion{
 	char* word_text;//chave
 	char* word_sugestion;
+	int length;
 	struct Sugestion* prox;
 } Sugestion;
 //----------------------
 
 //-----------Constante
+#define C_MAX_INT 32767
 #define c_alphabet_length 26
 #define c_hash 100
 #define c_custo 2 //Constante para controlar o custo de edição
@@ -27,18 +30,20 @@ typedef struct Sugestion{
 //---------------Variaveis Globais
 No* gno_root_dictionary;//n�� raiz  ; gno =global No
 Sugestion* gno_sugestion;
-char* gs_last_word;//pegar na a palavra corrigida
-bool is_correction = false;//armengue
-int gi_custo = 0;
 char* gs_palavra;//Salva a palavra exatamente como foi escrita
+char* gs_sugestion_word;
+int gi_custo,gi_custo_aux; //variavel pra 'podar' o lenvenstein distance
 
 //--------------------------------
 
-char type_correction(char t){
-	if(gi_custo<c_custo)
-		return t;
-	else
-		return 'F';
+
+inline int MIN(int x,int y,int z){
+
+	int result = x;
+	if (result > y) result = y;
+	if (result > z) result = z;
+
+	return result;
 }
 
 char* append(char* a_str, char a_c) {
@@ -99,7 +104,7 @@ char* append_pos(char* a_str, char a_c, int pos){
 		int len = strlen(a_str);
 		if(len-1>=pos){
 			char * aux;
-			aux = append(a_str,NULL); // allocar o 1 espaço a mais
+			aux = append(a_str,'\0'); // allocar o 1 espaço a mais
 			memmove(aux+pos+1, aux+pos, len-pos+1);
 			aux[pos]=a_c;
 			return aux;
@@ -235,7 +240,7 @@ char* exist_word(No* a_root, char* a_word){
 
 
 
-bool verify_word(No** a_root, char* a_word, int a_line, char a_tipo){//exist_word 2.0
+bool verify_word(No** a_root, char* a_word, int a_line){//exist_word 2.0
 	//-------------------------------------------------------------
 	//Retorno:
 	//			bool : true = palavra achada
@@ -255,8 +260,8 @@ bool verify_word(No** a_root, char* a_word, int a_line, char a_tipo){//exist_wor
 		return false;
 	else if(a_word[0]=='\0')
 
-		if ((*a_root)->exists){//Se existe a palavra, marca a linha que foi encontrada
-			if (a_line != -1){
+		if ((*a_root)->exists!=NULL){//Se existe a palavra, marca a linha que foi encontrada
+
 				if((*a_root)->line == NULL){
 					(*a_root)->line = malloc(sizeof(int)*2);
 					(*a_root)->line[0] = a_line;
@@ -273,74 +278,114 @@ bool verify_word(No** a_root, char* a_word, int a_line, char a_tipo){//exist_wor
 					if((*a_root)->line[i-1]!=a_line)
 						(*a_root)->line[i] = a_line;
 				}
-			}
-			return true;
 
+			return true;
 		}else{
 			return false;
 		}
 
 	else
-		if (verify_word(&(*a_root)->sheet[a_word[0]-'a'],a_word+1,a_line,'I')){
-			//printf("%c\n",a_word[0]);
-			gs_last_word = append_pos(gs_last_word,a_word[0],0);
-			return true;
+		return verify_word(&(*a_root)->sheet[a_word[0]-'a'],a_word+1,a_line);
+}
+
+int LevenshteinDistance(char* s, char* t)
+{
+    // degenerate cases
+    if (strcmp(s,t)==0) return 0;
+    //if (s.Length == 0) return t.Length;
+    //if (t.Length == 0) return s.Length;
+
+    int slen = strlen(s);
+    int tlen = strlen(t);
+    int i,j;
+
+    // create two work vectors of integer distances
+    int* v0 = malloc(sizeof(int)*(tlen+1));//new int[t.Length + 1];
+    int* v1 = malloc(sizeof(int)*(tlen+1));
+
+    // initialize v0 (the previous row of distances)
+    // this row is A[0][i]: edit distance for an empty s
+    // the distance is just the number of characters to delete from t
+    for (i = 0; i < tlen+1; i++)
+        v0[i] = i;
+
+    for (i = 0; i < slen; i++)
+    {
+        // calculate v1 (current row distances) from the previous row v0
+
+        // first element of v1 is A[i+1][0]
+        //   edit distance is delete (i+1) chars from s to match empty t
+        v1[0] = i + 1;
+
+        // use formula to fill in the rest of the row
+        for (j = 0; j < tlen; j++)
+        {
+            short int  cost = (s[i] == t[j]) ? 0 : 1;
+            v1[j + 1] = MIN(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost);
+
+        }
+
+        // copy v1 (current row) to v0 (previous row) for next iteration
+        for (j = 0; j < tlen+1; j++)
+            v0[j] = v1[j];
+    }
+
+    return v1[tlen];
+}
+
+char* correct_word(No* a_root, char* a_word){
+
+
+	if(a_root == NULL)//Caso a arvore esteja nula, só ocorre na inicialização
+		return NULL;
+	else {
+
+		int i;
+		if(a_root->exists!=NULL){
+			gi_custo_aux = LevenshteinDistance(a_word,gs_palavra);
+			if(gi_custo_aux<gi_custo){
+				gi_custo = gi_custo_aux;
+				gs_sugestion_word = a_root->exists;
+			}
+			if(gi_custo==1)
+				return gs_sugestion_word;
 		}
-		else{
-			int i;
 
-			//INSERCAO
-			if(a_tipo =='I'){
-				gi_custo++;
-				char* aux = NULL;
-				for (i=0;i<c_alphabet_length;i++){
-					aux = append_pos(a_word,'a'+i,0);
-					if((*a_root)->sheet[aux[0]-'a']!=NULL) // evitar 'empilhamento' desnecessario
-						if(verify_word(&(*a_root)->sheet[aux[0]-'a'],aux+1,-1,type_correction('I'))){ // TODO não insere na ultima posição
-				//			printf("%c\n",aux[0]);
-							gs_last_word = append_pos(gs_last_word,aux[0],0);
-							is_correction = true;
-							return true;
-						}
-				}
-				a_tipo = 'T';
-				gi_custo--;
-			}
+		for(i = 0 ; i<c_alphabet_length; i++)//Continue procurando em cada filho
+			if(a_root->sheet[i]!= NULL)
+				if(correct_word(a_root->sheet[i],append(a_word,'a'+i))!=NULL)
+					return gs_sugestion_word;
+	}
 
-			//TROCA
-			if(a_tipo == 'T'){
-				gi_custo++;
-				for (i=0;i<c_alphabet_length;i++){
-					if((*a_root)->sheet[i]!=NULL)
-						if(verify_word(&(*a_root)->sheet[i],a_word+2,-1,type_correction('T'))){
-					//		printf("%c\n",'a'+i);
-							gs_last_word = append_pos(gs_last_word,'a'+i,0);
-							is_correction = true;
-							return true;
-						}
-				}
-				a_tipo = 'D';
-				gi_custo--;
-			}
-
-			//DELETE
-			if(a_tipo == 'D'){
-				gi_custo++;
-				if(a_word[1]!='\0')
-					if(verify_word(&(*a_root)->sheet[a_word[1]-'a'],a_word+1,-1,type_correction('D'))){
-						//printf("%c",a_word[1]);
-						gs_last_word = append_pos(gs_last_word,a_word[1],0);
-						is_correction = true;
-						return true;
-					}
-				gi_custo--;
-			}
-
-
-			return false;
-		}
+	return NULL;
 
 }
+
+void insert_word_in_list(char* str,int len){
+	Sugestion* aux = gno_sugestion;
+	gs_palavra = str;
+	gi_custo = C_MAX_INT;
+	if(aux==NULL){
+		gno_sugestion = (Sugestion *)malloc(sizeof(Sugestion));
+		gno_sugestion->word_text = str;
+		gno_sugestion->length = len;
+
+		correct_word(gno_root_dictionary,NULL);
+		gno_sugestion->word_sugestion = gs_sugestion_word;
+
+	}else{
+		while (aux->prox != NULL)
+			aux = aux->prox;
+
+		aux = (Sugestion *)malloc(sizeof(Sugestion));
+		aux->word_text = str;
+		aux->length = len;
+		aux->word_sugestion = correct_word(gno_root_dictionary,NULL);
+
+	}
+	printf("%s %s\n",gs_palavra,gs_sugestion_word);
+}
+
 
 bool is_letter(char a_c){
 	//-------------------------------------------------------------
@@ -469,6 +514,7 @@ void initialize_text(char* a_name_file){
 			char* ls_str;
 			long int li_file_size;
 			int li_line = 1;
+			int len;
 
 			fseek(lf_file, 0L, SEEK_END);//deslocar o curso para o fim para poder pegar seu tamanho maximo
 			li_file_size = ftell(lf_file);//pegar o tamanho do arquivo
@@ -479,12 +525,13 @@ void initialize_text(char* a_name_file){
 
 
 			while(lc_c!=EOF && ftell(lf_file) < li_file_size){//Loop para pegar o texto no arquivo
+				len = 0;
 				gs_palavra = NULL;
 				ls_str=NULL;
 				fscanf(lf_file,"%c",&lc_c);
 
-
 				while (is_letter(lc_c) && ftell(lf_file) < li_file_size){
+					len++;
 					gs_palavra = append(gs_palavra,lc_c);
 					lc_c = lower(lc_c);
 					ls_str = append(ls_str,lc_c);
@@ -493,20 +540,15 @@ void initialize_text(char* a_name_file){
 				}
 				if(ls_str!=NULL){
 					if(!is_letter(lc_c)){
-						gs_last_word = NULL;
-						is_correction = false;
-						verify_word(&gno_root_dictionary,ls_str,li_line,'I');
-						if(is_correction)
-							printf("%s %s\n",gs_palavra,gs_last_word);
+						if(!verify_word(&gno_root_dictionary,ls_str,li_line))
+							insert_word_in_list(ls_str,len);
 					}else{//Necessário devido ao final de texto(código exclui a ultima letra por causa do while)
+						len++;
 						gs_palavra = append(gs_palavra,lc_c);
 						lc_c = lower(lc_c);
 						ls_str = append(ls_str,lc_c);
-						gs_last_word = NULL;
-						is_correction = false;
-						verify_word(&gno_root_dictionary,ls_str,li_line,'I');
-						if(is_correction)
-							printf("%s %s\n",gs_palavra,gs_last_word);
+						if(!verify_word(&gno_root_dictionary,ls_str,li_line))
+							insert_word_in_list(ls_str,len);
 					}
 				}
 				if(lc_c == '\n'){
@@ -532,11 +574,6 @@ int main(int argc, char **argv) {
 
 	if(argc >= 1){//Mudar dps para 3<<<<<<<<<<<<<<<<<<<<<<<<<
 
-		//insert_hash("aew","ae");
-		//insert_hash("vaca","abra");
-		//insert_hash("aew","ae");
-		//insert_hash("cada","casa");
-
 		initialize_dictionary(argv[1]);
 		initialize_text(argv[2]);
 
@@ -548,12 +585,7 @@ int main(int argc, char **argv) {
 
 
 		exist_word(gno_root_dictionary,NULL);//Mostra a saída
-		/*int i;
-		insert_sugestion(gno_sugestion,"tesre",'T');
-		insert_sugestion(gno_sugestion,"teste",'S');
-		for(i=0;i<=gi_number_sugestion;i++){
-			printf("%s %s\n",gno_sugestion[i]->word_text,gno_sugestion[i]->word_sugestion);
-		}*/
+
 
 	}else{
 		printf("Bad arguments");
